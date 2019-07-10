93de08d83376297b0c84d6aaaa4f3ff493abada2
mahmoud <buttererror@gmail.com>
2019-07-10 18:34:04 +0200

    setting up socket.io and apply events

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-const port = 80;
+const port = 3000;
 
-app.listen(port, () => {console.log("listening on port", port)});
+let count = 0;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    count++;
+    io.emit('countUpdated', count);
+  });
+
+});
+
+server.listen(port, () => {console.log("listening on port", port)});
