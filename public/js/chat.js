3f6d634537221d7d195b52878107e8ffb827f1f0
mahmoud <buttererror@gmail.com>
2019-07-14 21:38:14 +0200

    broadcastring events

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,8 +4,13 @@ socket.on('message', (message) => {
     console.log(message);
 });
 
+socket.on('user connected', () => {
+    console.log('new user connected');
+});
+
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault();
     let message = e.target.elements.message.value;
     socket.emit('sendMessage', message);
+
 });
--- src/index.js
+++ src/index.js
@@ -17,10 +17,15 @@ const port = 3000;
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.emit("message", "welcome");
+  socket.broadcast.emit('user connected');
   socket.on('sendMessage', (message) => {
     io.emit('message', message);
     console.log(message);
   });
+  socket.on('disconnect', () => {
+    io.emit('message', 'user disconnected');
+  });
+    
 });
 
 server.listen(port, () => {console.log("listening on port", port)});
